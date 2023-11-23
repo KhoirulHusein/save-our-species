0a921d730aedfd64ff24c1838acf85981604ab11
sparkle31 <4521210007@univpancasila.ac.id>
2023-11-23 23:26:07 +0700

    build: install dependencies router with configure  routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPages from './components/Pages/LandingPages';
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPages />,
+  },
+  {
+    path: '/down',
+    element: <App />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
