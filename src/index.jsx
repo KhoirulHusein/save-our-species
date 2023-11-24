79cca923d166c784d386700e388fff6e5cd9e1e1
sparkle31 <4521210007@univpancasila.ac.id>
2023-11-24 23:42:08 +0700

    Refactor React Router into separate files and deploy components lazily

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LandingPages from './components/Pages/LandingPages';
-import App from './App';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LandingPages />,
-  },
-  {
-    path: '/down',
-    element: <App />,
-  },
-]);
+import Routes from './routes/Routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Routes />
   </React.StrictMode>,
 );
