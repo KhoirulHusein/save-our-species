80bb65cf8b426a0076a3d1cb980201e8a4f09aa6
sparkle31 <4521210007@univpancasila.ac.id>
2023-11-26 13:50:07 +0700

    feat: Create LandingPages component for the landing page layout

--- src/components/Pages/LandingPages.jsx
+++ src/components/Pages/LandingPages.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import HeroSection from '../Layouts/LandingPage/HeroSections';
 import ExploreAnimal from '../Layouts/LandingPage/ExploreAnimals';
+import Jumroton from '../Layouts/Jumroton';
 
 function LandingPages() {
   return (
-    <>
-      <HeroSection variant="flex justify-start items-center h-screen relative" />
-      <ExploreAnimal />
-    </>
+    <div className="bg-black-900 flex flex-col font-ubuntu items-center justify-start mx-auto w-full">
+      <div className="flex flex-col items-center justify-start w-full">
+        <div className="min-h-screen md:px-5 relative w-full">
+          <HeroSection />
+        </div>
+        <ExploreAnimal />
+        <Jumroton />
+      </div>
+    </div>
   );
 }
 
