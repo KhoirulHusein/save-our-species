5da7b9b972305dca32201e5d22d6c24691310665
sparkle31 <4521210007@univpancasila.ac.id>
2023-11-30 14:30:58 +0700

    BREAKING CHANGE! Refactor The usage of the Button component may require updates to conform to the new prop structure.

--- src/components/Elements/Button/Buttons.jsx
+++ src/components/Elements/Button/Buttons.jsx
@@ -1,15 +1,65 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/button-has-type */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Button(props) {
-  const { buttonText } = props;
+const shapes = { circle: 'rounded-[50%]', round: 'rounded-[7px]' };
+const variants = {
+  fill: {
+    white_A700: 'bg-white-A700 text-black-900',
+    light_green_800: 'bg-light_green-800 text-white-A700',
+    green_400: 'bg-green-400 text-white-A700',
+    red_A700_01: 'bg-red-A700_01 text-white-A700',
+    blue_gray_100: 'bg-blue_gray-100',
+  },
+};
+const sizes = {
+  xs: 'p-[7px]',
+  sm: 'px-[9px] py-[11px]',
+  md: 'p-[15px]',
+  lg: 'p-[18px]',
+  xl: 'p-[33px] sm:px-5',
+};
+
+const Button = ({
+  children,
+  className = '',
+  leftIcon,
+  rightIcon,
+  shape = '',
+  size = '',
+  variant = '',
+  color = '',
+  ...restProps
+}) => {
   return (
-    <div className="w-fit px-6 py-4 bg-[#606C38] rounded-[7px] justify-center items-center gap-2.5 inline-flex">
-      <button className="text-center text-white-A700 text-base font-medium" type="button">
-        {buttonText}
-      </button>
-    </div>
+    <button
+      className={`${className} ${(shape && shapes[shape]) || ''} ${
+        (size && sizes[size]) || ''
+      } ${(variant && variants[variant]?.[color]) || ''}`}
+      {...restProps}
+    >
+      {!!leftIcon && leftIcon}
+      {children}
+      {!!rightIcon && rightIcon}
+    </button>
   );
-}
+};
 
-export default Button;
+Button.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+  shape: PropTypes.oneOf(['circle', 'round']),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl']),
+  variant: PropTypes.oneOf(['fill']),
+  color: PropTypes.oneOf([
+    'white_A700',
+    'light_green_800',
+    'green_400',
+    'red_A700_01',
+    'blue_gray_100',
+  ]),
+};
+
+export { Button };
