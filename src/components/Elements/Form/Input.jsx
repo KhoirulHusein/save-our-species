3538c60ad33c931eac3de8a0b452eccaa185a549
sparkle31 <4521210007@univpancasila.ac.id>
2023-12-04 00:40:14 +0700

    fix: label with disable and select with linebreaking

--- src/components/Elements/Form/Input.jsx
+++ src/components/Elements/Form/Input.jsx
@@ -12,6 +12,7 @@ const Input = React.forwardRef(
       children,
       onChange,
       inputId,
+      isLabel,
       labelName,
       ...restProps
     },
@@ -23,11 +24,13 @@ const Input = React.forwardRef(
 
     return (
       <>
-        <label htmlFor={`${inputId}`} className="block mb-2 text-sm font-medium text-white-900 dark:text-white">{labelName}</label>
+        {isLabel && (
+          <label htmlFor={`${inputId}`} className="block mb-2 text-sm font-medium text-white-900 dark:text-white">{labelName}</label>
+        )}
         <input
           ref={ref}
           id={inputId}
-          className={`${className} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          className={className}
           type={type}
           name={name}
           onChange={handleChange}
