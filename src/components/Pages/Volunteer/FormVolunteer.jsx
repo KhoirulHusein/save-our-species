2a43fa1d6e145aa4c6d7381a6ffe51b3dd4fdc3e
Siti Rahmah <114862886+strahma-ti@users.noreply.github.com>
2023-12-03 02:45:33 +0000

    add FormVolunteer page

---
+++ src/components/Pages/Volunteer/FormVolunteer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeroSection from '../../Layouts/FormVolunteer/HeroSection';
+import FormSection from '../../Layouts/FormVolunteer/FormSection';
+import ApplyButton from '../../Layouts/FormVolunteer/ApplyButton';
+import Footer from '../../Fragments/Footer/Footer';
+import Navbar from '../../Fragments/Navbar/Navbar';
+
+const FormVolunteer = () => {
+  return (
+    <>
+      <div className="bg-gray-900 flex flex-col font-ubuntu items-center justify-start mx-auto w-full">
+        <Navbar />
+        <div className="flex flex-col justify-start w-full">
+          <HeroSection />
+          <FormSection />
+        </div>
+      </div>
+      <ApplyButton />
+      <Footer />
+    </>
+  );
+};
+
+export default FormVolunteer;
