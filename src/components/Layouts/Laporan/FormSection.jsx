b022b2eb9553eb4eafef64c1e5d65f683c3ed50a
Siti Rahmah <114862886+strahma-ti@users.noreply.github.com>
2023-12-05 10:14:27 +0000

    fix: text-area

--- src/components/Layouts/Laporan/FormSection.jsx
+++ src/components/Layouts/Laporan/FormSection.jsx
@@ -30,13 +30,13 @@ function FormSection() {
         size="sm"
       />
       <Text
-        className="md:ml-[0] mt-10 w-full text-white-A700 text-xl md:text-[15px]"
+        className="md:ml-[0] mt-10 md:mt-[10px] w-full text-white-A700 text-xl md:text-[15px]"
         size="txtUbuntuBold20WhiteA700"
       >
         Prioritas
       </Text>
       <SelectBox
-        className="md:ml-[0] pt-1 pb-1 w-full md:text-[15px]"
+        className="md:ml-[0] pt-[5px] pb-[5px] mt-[10px] w-full md:text-[15px]"
         placeholderClassName="text-black-900"
         indicator={<FontAwesomeIcon icon={faCaretDown} className="h-[25px] w-2.5 text-green-800 cursor-pointer" />}
         isMulti={false}
@@ -50,7 +50,7 @@ function FormSection() {
         variant="fill"
       />
       <Text
-        className="md:ml-[0] mt-10 w-full text-white-A700 text-xl md:text-[15px]"
+        className="md:ml-[0] mt-10 md:mt-[10px] w-full text-white-A700 text-xl md:text-[15px]"
         size="txtUbuntuBold20WhiteA700"
       >
         Tempat Kejadian
@@ -65,7 +65,7 @@ function FormSection() {
         size="sm"
       />
       <Text
-        className="md:ml-[0] mt-10 w-full text-white-A700 text-xl md:text-[15px]"
+        className="md:ml-[0] mt-10 md:mt-[10px] w-full text-white-A700 text-xl md:text-[15px]"
         size="txtUbuntuBold20WhiteA700"
       >
         Ciri-ciri pelaku (Nama pelaku jika tahu)
@@ -80,19 +80,16 @@ function FormSection() {
         size="sm"
       />
       <Text
-        className="md:ml-[0] mt-10 w-full text-white-A700 text-xl md:text-[15px]"
+        className="md:ml-[0] mt-10 md:mt-[10px] w-full text-white-A700 text-xl md:text-[15px]"
         size="txtUbuntuBold20WhiteA700"
       >
         Deskripsi Kejadian
       </Text>
-      <Input
-        labelName=""
-        name="namaPelapor"
+      <textarea
+        name="deskripsiKejadian"
         placeholder="Isi disini...."
-        className="md:ml-[0] ml-0 p-0 w-full mt-[10px] h-48"
-        wrapClassName="border border-blue_gray-900_01 border-solid flex mt-[19px] mx-auto w-full"
-        shape="round"
-        size="sm"
+        className="md:ml-[0] ml-0 p-2 w-full mt-[5px] h-48 border border-blue_gray-900_01 border-solid flex mt-[19px] mx-auto w-full rounded-sm md:text-[15px]"
+        rows="4"
       />
     </div>
   );
