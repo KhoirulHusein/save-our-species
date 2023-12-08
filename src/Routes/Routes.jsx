199189c9c8e241850461d4a90ab1a59f253b91fe
kevinxzuto <119093242+kevinxzuto@users.noreply.github.com>
2023-12-08 13:14:39 +0000

    Update Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const Landingpage = React.lazy(() => import('../components/Pages/LandingPage/LandingPages'));
 const App = React.lazy(() => import('../App'));
+const ListAnimals = React.lazy(() => import('../components/Pages/ListAnimals/ListAnimals'));
+const DetailAnimals = React.lazy(() => import('../components/Pages/DetailAnimals/DetailAnimals'));
 
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
-          <Route path="/" element={<Landingpage />} />
-          <Route path="/maintance" element={<App />} />
+          <Route path="/animals/details/:id" element={<DetailAnimals />} />
+          <Route path="/animals" element={<ListAnimals />} />
+          <Route path="/" element={<App />} />
         </Routes>
       </Router>
     </React.Suspense>
