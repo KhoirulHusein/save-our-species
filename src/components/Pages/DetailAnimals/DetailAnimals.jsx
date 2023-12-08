df64440a18bd93e713f58f5d55228565218beed2
kevinxzuto <119093242+kevinxzuto@users.noreply.github.com>
2023-12-08 13:13:41 +0000

    Add list and detail animals page

---
+++ src/components/Pages/DetailAnimals/DetailAnimals.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DetailHewan from '../../Elements/layouts/DetailHewan/DetailHewan';
+
+function DetailAnimals() {
+  return (
+    <DetailHewan />
+  );
+}
+
+export default DetailAnimals;
