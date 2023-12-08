064dcfba03bb398bb3a1a50b45f3f7bc684fb002
Siti Rahmah <114862886+strahma-ti@users.noreply.github.com>
2023-12-08 11:15:32 +0000

    add: routes lapor & volunteer

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,6 +9,10 @@ const DetailDonation = React.lazy(() => import('../components/Pages/DetailDonati
 const FinishPayment = React.lazy(() => import('../components/Pages/DetailDonation/Finish'));
 const UnfinishPayment = React.lazy(() => import('../components/Pages/DetailDonation/Unfinish'));
 const FailsPayment = React.lazy(() => import('../components/Pages/DetailDonation/Fails'));
+const Laporan = React.lazy(() => import('../components/Pages/Laporan/Laporan'));
+const LaporanSucces = React.lazy(() => import('../components/Pages/Laporan/LaporanSucces'));
+const FormVolunteer = React.lazy(() => import('../components/Pages/Volunteer/FormVolunteer'));
+const FormVolunteerSucces = React.lazy(() => import('../components/Pages/Volunteer/FormVolunteerSucces'));
 
 const ProjectRoutes = () => {
   return (
@@ -21,6 +25,10 @@ const ProjectRoutes = () => {
           <Route path="/finishspayment" element={<FinishPayment />} />
           <Route path="/unfinishpayment" element={<UnfinishPayment />} />
           <Route path="/failspayment" element={<FailsPayment />} />
+          <Route path="/lapor" element={<Laporan />} />
+          <Route path="/laporansucces" element={<LaporanSucces />} />
+          <Route path="/formvolunteer" element={<FormVolunteer />} />
+          <Route path="/formvolunteersucces" element={<FormVolunteerSucces />} />
         </Routes>
       </Router>
     </React.Suspense>
