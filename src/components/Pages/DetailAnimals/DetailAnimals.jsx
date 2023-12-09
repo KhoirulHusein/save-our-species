96c096bafa32259a24db844a384c3774ac6852d8
salsa <75343390+salsaamputeri@users.noreply.github.com>
2023-12-09 05:24:36 +0000

    add navbar+footer+margin

--- src/components/Pages/DetailAnimals/DetailAnimals.jsx
+++ src/components/Pages/DetailAnimals/DetailAnimals.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import DetailHewan from '../../Layouts/DetailHewan/DetailHewan';
+import Footer from '../../Fragments/Footer/Footer';
+import Navbar from '../../Fragments/Navbar/Navbar';
 
 function DetailAnimals() {
   return (
-    <DetailHewan />
+    <div className="bg-gray-900 text-white-A700 font-ubuntu justify-start mx-auto w-full">
+      <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingBottom: '16rem', paddingTop: '5rem' }}>
+        {' '}
+        <Navbar />
+        <DetailHewan />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/Pages/ListAnimals/ListAnimals.jsx
+++ src/components/Pages/ListAnimals/ListAnimals.jsx
@@ -6,10 +6,14 @@ import Navbar from '../../Fragments/Navbar/Navbar';
 function ListAnimals() {
   return (
     <div className="bg-gray-900 text-white-A700 font-ubuntu justify-start mx-auto w-full">
-      <Navbar />
-      <ListCards />
+      <div style={{ textAlign: 'center', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', paddingBottom: '16rem', paddingTop: '3rem' }}>
+        {' '}
+        <Navbar />
+        <ListCards />
+      </div>
       <Footer />
     </div>
+
   );
 }
 
